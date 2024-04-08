2e3472b0a23f5d119ee8db2e632948dd562d6378
Lucas Gauvain <lucasgauvain@gmail.com>
2024-04-08 15:57:05 +0200

    Refactor main.go to separate image downloading and hashing logic

--- img_diff/main.go
+++ img_diff/main.go
@@ -12,24 +12,9 @@ import (
 	"strings"
 )
 
-func main() {
-	// Création d'un logger pour écrire les messages d'erreur dans stderr
-	logger := log.New(os.Stderr, "", log.LstdFlags)
-
-	// Ajout du flag pour passer une liste d'URLs en argument
-	urlsFlag := flag.String("urls", "", "Liste d'URLs séparées par des virgules")
-	flag.Parse()
-
-	// Parsing de la chaîne d'URLs
-	var urls []string
-	if *urlsFlag != "" {
-		urls = strings.Split(*urlsFlag, ",")
-	}
-
-	// Téléchargement des images depuis les URLs
+func downloadImages(urls []string, logger *log.Logger) []string {
 	images := []string{}
 	for i, url := range urls {
-		// Téléchargement de l'image depuis l'URL
 		resp, err := http.Get(url)
 		if err != nil {
 			logger.Printf("Erreur lors du téléchargement de %s : %v\n", url, err)
@@ -37,14 +22,12 @@ func main() {
 		}
 		defer resp.Body.Close()
 
-		// Lecture des données de l'image
 		data, err := io.ReadAll(resp.Body)
 		if err != nil {
 			logger.Printf("Erreur lors de la lecture de %s : %v\n", url, err)
 			continue
 		}
 
-		// Écriture de l'image sur le disque
 		filename := fmt.Sprintf("image_%d.jpg", i+1)
 		err = os.WriteFile(filename, data, 0644)
 		if err != nil {
@@ -52,17 +35,18 @@ func main() {
 			continue
 		}
 
-		// Ajout du nom de fichier à la liste des images téléchargées
 		images = append(images, filename)
 	}
+	return images
+}
 
-	// Calcul des hashes SHA-256 pour chaque image
+func calculateHashes(images []string, logger *log.Logger) []string {
 	hashes := make([]string, len(images))
 	for i := 0; i < len(images); i++ {
 		file, err := os.Open(images[i])
 		if err != nil {
 			logger.Printf("Erreur lors de l'ouverture de %s : %v\n", images[i], err)
-			return
+			return nil
 		}
 		defer file.Close()
 
@@ -71,14 +55,15 @@ func main() {
 		_, err = reader.WriteTo(hash)
 		if err != nil {
 			logger.Printf("Erreur lors de la lecture de %s : %v\n", images[i], err)
-			return
+			return nil
 		}
 
 		hashes[i] = fmt.Sprintf("%x", hash.Sum(nil))
 	}
+	return hashes
+}
 
-	// Recherche de l'image unique
-	var uniqueImage string
+func findUniqueImage(images []string, hashes []string) string {
 	for i := 0; i < len(images); i++ {
 		isDuplicate := false
 		for j := 0; j < len(images); j++ {
@@ -88,12 +73,28 @@ func main() {
 			}
 		}
 		if !isDuplicate {
-			uniqueImage = images[i]
-			break
+			return images[i]
 		}
 	}
+	return ""
+}
 
-	// Affichage de l'image unique ou d'un message d'erreur si aucune image unique n'est trouvée
+func main() {
+	logger := log.New(os.Stderr, "", log.LstdFlags)
+
+	urlsFlag := flag.String("urls", "", "Liste d'URLs séparées par des virgules")
+	flag.Parse()
+
+	var urls []string
+	if *urlsFlag != "" {
+		urls = strings.Split(*urlsFlag, ",")
+	}
+
+	images := downloadImages(urls, logger)
+	hashes := calculateHashes(images, logger)
+
+	uniqueImage := findUniqueImage(images, hashes)
+
 	if uniqueImage != "" {
 		fmt.Printf("L'image unique est : %s\n", uniqueImage)
 	} else {
