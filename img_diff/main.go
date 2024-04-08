45beb1f6b4757493c0033da55bb05e6e92903722
Lucas Gauvain <lucasgauvain@gmail.com>
2024-04-08 13:58:12 +0200

    Refactor main.go to use bufio.Reader for reading files and calculate SHA256 hash

--- img_diff/main.go
+++ img_diff/main.go
@@ -4,27 +4,29 @@ import (
 	"bufio"
 	"crypto/sha256"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
+	logger := log.New(os.Stderr, "", log.LstdFlags)
+
 	images := []string{"image_1.jpg", "image_2.jpg", "image_3.jpg"}
 	hashes := make([]string, len(images))
 
 	for i := 0; i < len(images); i++ {
 		file, err := os.Open(images[i])
 		if err != nil {
-			fmt.Printf("Erreur lors de l'ouverture de %s : %v\n", images[i], err)
+			logger.Printf("Erreur lors de l'ouverture de %s : %v\n", images[i], err)
 			return
 		}
 		defer file.Close()
 
 		reader := bufio.NewReader(file)
 		hash := sha256.New()
-
 		_, err = reader.WriteTo(hash)
 		if err != nil {
-			fmt.Printf("Erreur lors de la lecture de %s : %v\n", images[i], err)
+			logger.Printf("Erreur lors de la lecture de %s : %v\n", images[i], err)
 			return
 		}
 
@@ -49,6 +51,6 @@ func main() {
 	if uniqueImage != "" {
 		fmt.Printf("L'image unique est : %s\n", uniqueImage)
 	} else {
-		fmt.Println("Aucune image unique trouvée.")
+		logger.Println("Aucune image unique trouvée.")
 	}
 }
